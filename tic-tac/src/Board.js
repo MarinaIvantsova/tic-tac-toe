67ff825b4362abb1dfef3095bd00af70537e429d
Marina Ivantsova <marineivantsova@yandex.ru>
2023-04-17 19:07:29 +0700

    Added the winner

--- tic-tac/src/Board.js
+++ tic-tac/src/Board.js
@@ -4,11 +4,12 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+
   const handleClick = (i) => {
-    const newSquares = [...squares];
-    if (newSquares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
+    const newSquares = [...squares];
     if (xIsNext) {
       newSquares[i] = "X";
     } else {
@@ -17,8 +18,19 @@ export default function Board() {
     setSquares(newSquares);
     setXIsNext(!xIsNext);
   };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `The winner is ${winner}`
+  }
+   else {
+    status = 'The next player:' + (xIsNext ? 'X': 'O')
+  }
+
   return (
     <>
+    <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onCLickHandler={() => handleClick(0)} />
         <Square value={squares[1]} onCLickHandler={() => handleClick(1)} />
@@ -44,4 +56,24 @@ function Square({ value, onCLickHandler }) {
       {value}
     </button>
   );
+}
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]; 
+    if (squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
